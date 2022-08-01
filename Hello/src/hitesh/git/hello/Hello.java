6909d28679d994c8f93b6086903a0605328c8b32
Hitesh Verma <hv51186@gmail.com>
2022-08-01 15:35:38 -0400

    first commit

---
+++ Hello/src/hitesh/git/hello/Hello.java
@@ -0,0 +1,10 @@
+package hitesh.git.hello;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Will try to put this in github");
+	}
+
+}
